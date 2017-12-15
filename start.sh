7d595d6c9dd2ed855eb341fe36e0ea3c1388bfd1
anubhavj02 <jainan@tcd.ie>
2017-12-15 22:18:33 +0000

    Commit 9: Commitng the start shell script

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Script file for executing the chat server on the machine and accept the first command line argument as port number
+
+echo "Name: Anubhav Jain, Student Id: 17310876"
+
+echo "Distributed File System"
+
+echo "Running the authentication Server"
+python authentication_server.py
+
+echo "Running the directory Server"
+python directory_service_server.py
+
+echo "Running the lock Server"
+python lock_server.py
